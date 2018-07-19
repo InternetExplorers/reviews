51f315980be6fa6d27985aaf0434bd9608658a97
Burke <burkegreen@gmail.com>
2018-07-18 19:12:00 -0700

    Added dependencies.

---
+++ client/index.jsx
@@ -0,0 +1,4 @@
+
+import Reviews from './components/reviews.jsx';
+
+window.Reviews = Reviews;
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+// const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  // context: __dirname + '/client/public',
+  entry: __dirname + '/client/index.jsx',
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env', 'react']
+          },
+        },
+      },
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname + './public'),
+    filename: 'bundle.js',
+  }
+};
