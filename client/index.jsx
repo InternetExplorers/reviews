43b889e15c8ac803ca08ad3a89d3d712ead45de4
Burke <burkegreen@gmail.com>
2018-07-18 19:24:20 -0700

    Updated webpack config.

--- client/index.jsx
+++ client/index.jsx
@@ -1,4 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import Reviews from './components/reviews.jsx';
 
-window.Reviews = Reviews;
+ReactDOM.render(<h1>hello</h1>, document.getElementById('app'));
---
+++ database/index.js
@@ -0,0 +1,13 @@
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host: 'localhost', 
+  user: 'student',
+  password: 'student',
+  database: 'chat'
+});
+
+connection.connect();
+
+
+module.exports = connection;
