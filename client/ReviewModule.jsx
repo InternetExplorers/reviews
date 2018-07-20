cba9d71c683611ca4c37faeac660a8faf297a401
Burke Green <burkegreen@gmail.com>
2018-07-20 14:11:15 -0700

    Update ReviewModule.jsx

--- client/ReviewModule.jsx
+++ client/ReviewModule.jsx
@@ -12,7 +12,6 @@ export default class ReviewModule extends React.Component {
   }
 
   render() {
-    //const { unhappy } = this.state;  example destructuring
     return (
       <div>
         <TopBar />
