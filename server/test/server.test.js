36043dff9f5040e12ade43e0a8e7ba94a32b0fa3
Burke <burkegreen@gmail.com>
2018-07-26 09:30:17 -0700

    Updated to test on CircleCi.

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -2,10 +2,9 @@ const request = require('request');
 
 describe('Basic server test', () => {
   it('fetches data from api', (done) => {
-    request('http://localhost:3004/locations/33/reviews', (error, response, body) => {
+    request('http://localhost:3004/locations/33/reviews', async (error, response, body) => {
       const reviews = JSON.parse(body);
       expect(Object.keys(reviews[0])).toContain('id');
-      // expect(Object.keys(body[0])).toContain('id');
       done();
     });
   });
