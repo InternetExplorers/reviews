68c853edf9b02ce0e3800c91321e89cb670d020a
Burke <burkegreen@gmail.com>
2018-07-31 16:46:06 -0700

    URL now requires number for business ID.  e.g. http://localhost:3004/42

--- client/index.jsx
+++ client/index.jsx
@@ -2,4 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ReviewModule from './ReviewModule.jsx';
 
-ReactDOM.render(<ReviewModule />, document.getElementById('app'));
+const id = Number(window.location.pathname.slice(1, window.location.pathname.length - 1));
+
+ReactDOM.render(<ReviewModule id={id} />, document.getElementById('Burke'));
