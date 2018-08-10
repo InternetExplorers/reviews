d36f218b32a6a9750ef33ace6b98b40c36abf406
Kristie Desiree <kly5@mail.sfsu.edu>
2018-08-10 10:47:09 -0700

    Refactor the function that writes out to a CSV

--- database/recordGenerator.js
+++ database/recordGenerator.js
@@ -1,17 +1,11 @@
 const faker = require('faker');
 const fs = require('file-system');
+
 let userRecordId = 1;
 let reviewRecordId = 1;
 const randomNumber = (min, limit) => Math.floor(Math.random() * (limit - min)) + min;
 
-/*
-name
-city
-friends
-photos
-reviews
-avatar
-*/
+/* name, city, friends, photos, reviews, avatar */
 const createChunkedUserRecordSet = (chunkSize) => {
   let result = '';
   for (let i = 0; i <= chunkSize; i += 1) {
@@ -21,13 +15,7 @@ const createChunkedUserRecordSet = (chunkSize) => {
   return result;
 };
 
-/*
-reviewText
-stars
-date
-userWhoWroteIt (id is between 1 and 20million)
-locationItsAbout
-*/
+/* reviewText, stars, date, userWhoWroteIt (id is between 1 and 20million), locationItsAbout */
 const createChunkedReviewRecordSet = (chunkSize) => {
   let result = '';
   for (let i = 0; i <= chunkSize; i += 1) {
@@ -37,17 +25,28 @@ const createChunkedReviewRecordSet = (chunkSize) => {
   return result;
 };
 
-for (let j = 1; j < 201; j += 1) { //numfiles will be 1-200 for a total of 20,000,000 records
-  for (let i = 0; i < 20; i += 1) { 
-    fs.appendFileSync('userData' + String(j) +'.txt', createChunkedUserRecordSet(5000)); //can increase this for speed 5k-10k
+const writeToFile = (numFiles, recordsPerChunk, chunksPerFile, recordGen, fileName) => {
+  for (let i = 1; i <= numFiles; i += 1) {
+    for (let j = 1; j <= chunksPerFile; j += 1) {
+      fs.appendFileSync(`${fileName}${i}.txt`, recordGen(recordsPerChunk));
+    }
+    console.log(`The data was appended to file ${fileName}${i}!`);
   }
-  console.log(`The userdata was appended to file ${j}!`);
-}
+};
 
-for (let j = 1; j < 501; j += 1) { //numfiles will be 1-500 for a total of 100,000,000 records
-  for (let i = 0; i < 20; i += 1) {
-    fs.appendFileSync('reviewData' + String(j) +'.txt', createChunkedReviewRecordSet(10000)); //can increase this for speed 5k-10k
-  }
-  console.log(`The record data was appended to file ${j}!`);
-}
+writeToFile(2, 5000, 20, createChunkedUserRecordSet, 'test');
 
+
+// for (let j = 1; j < 201; j += 1) { //numfiles will be 1-200 for a total of 20,000,000 records
+//   for (let i = 0; i < 20; i += 1) { 
+//     fs.appendFileSync(`userData${String(j)}.txt`, createChunkedUserRecordSet(5000)); //can increase this for speed 5k-10k
+//   }
+  
+// }
+
+// for (let j = 1; j < 501; j += 1) { //numfiles will be 1-500 for a total of 100,000,000 records
+//   for (let i = 0; i < 20; i += 1) {
+//     fs.appendFileSync('reviewData' + String(j) + '.txt', createChunkedReviewRecordSet(10000)); //can increase this for speed 5k-10k
+//   }
+//   console.log(`The record data was appended to file ${j}!`);
+// }
