652115574e3ec2174a12209dc1fd6c4daee00c57
Kristie Desiree <kly5@mail.sfsu.edu>
2018-08-10 11:26:12 -0700

    Refactor to prevent code repetition

--- database/recordGenerator.js
+++ database/recordGenerator.js
@@ -5,48 +5,34 @@ let userRecordId = 1;
 let reviewRecordId = 1;
 const randomNumber = (min, limit) => Math.floor(Math.random() * (limit - min)) + min;
 
-/* name, city, friends, photos, reviews, avatar */
-const createChunkedUserRecordSet = (chunkSize) => {
+const createChunkedRecordSet = (chunkSize, recordType) => {
   let result = '';
-  for (let i = 0; i <= chunkSize; i += 1) {
-    result += `${userRecordId}, ${faker.name.findName()}, ${faker.address.city()}, ${randomNumber(0, 500)}, ${randomNumber(0, 5000)}, ${randomNumber(0, 2000)}, ${faker.internet.avatar()} \n`;
-    userRecordId += 1;
+  for (let i = 0; i < chunkSize; i += 1) {
+    if (recordType === 'user') {
+      result += `${userRecordId}, ${faker.name.findName()}, ${faker.address.city()}, ${randomNumber(0, 500)}, ${randomNumber(0, 5000)}, ${randomNumber(0, 2000)}, ${faker.internet.avatar()} \n`;
+      userRecordId += 1;
+    } else if (recordType === 'review') {
+      result += `${reviewRecordId}, ${faker.lorem.paragraph()}, ${randomNumber(1, 6)}, ${randomNumber(1, 13)}-${randomNumber(1, 31)}-${randomNumber(2005, 2018)}, ${randomNumber(1, 20000000)}, ${randomNumber(1, 100000000)} \n`;
+      reviewRecordId += 1;
+    }
   }
   return result;
 };
 
-/* reviewText, stars, date, userWhoWroteIt (id is between 1 and 20million), locationItsAbout */
-const createChunkedReviewRecordSet = (chunkSize) => {
-  let result = '';
-  for (let i = 0; i <= chunkSize; i += 1) {
-    result += `${reviewRecordId}, ${faker.lorem.paragraph()}, ${randomNumber(1, 6)}, ${randomNumber(1, 13)}-${randomNumber(1, 31)}-${randomNumber(2005, 2018)}, ${randomNumber(1, 20000000)}, ${randomNumber(1, 100000000)} \n`;
-    reviewRecordId += 1;
-  }
-  return result;
-};
-
-const writeToFile = (numFiles, recordsPerChunk, chunksPerFile, recordGen, fileName) => {
+const writeToFile = (numFiles, recordsPerChunk, chunksPerFile, recordType, fileName) => {
   for (let i = 1; i <= numFiles; i += 1) {
     for (let j = 1; j <= chunksPerFile; j += 1) {
-      fs.appendFileSync(`${fileName}${i}.txt`, recordGen(recordsPerChunk));
+      fs.appendFileSync(`${fileName}${i}.txt`, createChunkedRecordSet(recordsPerChunk, recordType));
     }
     console.log(`The data was appended to file ${fileName}${i}!`);
   }
 };
 
-writeToFile(2, 5000, 20, createChunkedUserRecordSet, 'test');
+// createChunkedRecordSet(5000, 'user');
+
+//remember to write to file
+writeToFile(2, 5000, 20, 'user', 'user');
+writeToFile(2, 5000, 20, 'review', 'review');
 
 
-// for (let j = 1; j < 201; j += 1) { //numfiles will be 1-200 for a total of 20,000,000 records
-//   for (let i = 0; i < 20; i += 1) { 
-//     fs.appendFileSync(`userData${String(j)}.txt`, createChunkedUserRecordSet(5000)); //can increase this for speed 5k-10k
-//   }
-  
-// }
 
-// for (let j = 1; j < 501; j += 1) { //numfiles will be 1-500 for a total of 100,000,000 records
-//   for (let i = 0; i < 20; i += 1) {
-//     fs.appendFileSync('reviewData' + String(j) + '.txt', createChunkedReviewRecordSet(10000)); //can increase this for speed 5k-10k
-//   }
-//   console.log(`The record data was appended to file ${j}!`);
-// }
