98a23fd2e6e1b136a7fa3fd91349711746ea2894
Kristie Desiree <kly5@mail.sfsu.edu>
2018-08-12 20:20:06 -0700

    Generate records without id field

--- database/recordGenerator.js
+++ database/recordGenerator.js
@@ -2,18 +2,37 @@ const faker = require('faker');
 const fs = require('file-system');
 
 let userRecordId = 1;
-let reviewRecordId = 1;
+// let reviewRecordId = 1;
 const randomNumber = (min, limit) => Math.floor(Math.random() * (limit - min)) + min;
+
+const chooseInflatedRecords = (numFiles, percent) => {
+  const filesToInflate = [];
+  const numToInflate = (percent / 100) * numFiles;
+  let i = 0;
+  while (i < numToInflate) {
+    filesToInflate.push(randomNumber(1, numFiles));
+    i += 1;
+  }
+  return filesToInflate;
+};
 
 const createChunkedRecordSet = (chunkSize, recordType) => {
   let result = '';
+  const recordsToInflate = chooseInflatedRecords(10000000, 2);
   for (let i = 0; i < chunkSize; i += 1) {
     if (recordType === 'user') {
       result += `${userRecordId}, ${faker.name.findName()}, ${faker.address.city()}, ${randomNumber(0, 500)}, ${randomNumber(0, 5000)}, ${randomNumber(0, 2000)}, ${faker.internet.avatar()} \n`;
       userRecordId += 1;
     } else if (recordType === 'review') {
-      result += `${reviewRecordId}, ${faker.lorem.paragraph()}, ${randomNumber(1, 6)}, ${randomNumber(1, 13)}-${randomNumber(1, 31)}-${randomNumber(2005, 2018)}, ${randomNumber(1, 20000000)}, ${randomNumber(1, 100000000)} \n`;
-      reviewRecordId += 1;
+      if (i % 150 === 0) {
+        for (let j = 0; j < randomNumber(10, 20); j += 1) {
+          result += `${faker.lorem.paragraph()}, ${randomNumber(1, 6)}, ${randomNumber(1, 13)}-${randomNumber(1, 31)}-${randomNumber(2005, 2018)}, ${randomNumber(1, 20000000)}, ${recordsToInflate[0]} \n`;
+        }
+        recordsToInflate.pop();
+      } else {
+        result += `${faker.lorem.paragraph()}, ${randomNumber(1, 6)}, ${randomNumber(1, 13)}-${randomNumber(1, 31)}-${randomNumber(2005, 2018)}, ${randomNumber(1, 20000000)}, ${randomNumber(1, 100000000)} \n`; 
+      }
+      // reviewRecordId += 1;
     }
   }
   return result;
@@ -29,8 +48,10 @@ const writeToFile = (numFiles, recordsPerChunk, chunksPerFile, recordType, fileN
 };
 
 // 20M user records
-writeToFile(200, 5000, 20, 'user', 'userData');
+//writeToFile(200, 5000, 20, 'user', 'userData');
 
-// 100M review records
-writeToFile(500, 5000, 20, 'review', 'reviewData');
+// 30M review records
+// 2% of records will have exceptionally high number of reviews
+writeToFile(300, 5000, 20, 'review', 'reviewData');
+
 
