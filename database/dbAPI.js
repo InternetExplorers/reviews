740a076b6740ce60f36c292602cf3b3576685965
Kristie Desiree <kly5@mail.sfsu.edu>
2018-08-16 16:02:04 -0700

    Fill in CRUD routes with Postgres queries

--- database/dbAPI.js
+++ database/dbAPI.js
@@ -12,6 +12,41 @@ const getById = function getAllReviewsOfRestaurant(restID, callback) {
   });
 };
 
+// can put an optional id parameter so that the restaurant can be specified
+const addNewRecord = function getAllReviewsOfRestaurant(callback) {
+  const queryStr = `INSERT INTO reviews (message, stars, posted, userId, locId) VALUES ('Veritas vos liberabit. Veritas vos liberabit. Veritas vos liberabit.', 3, '11-11-2011', 123, 8900100);`;
+  db.query(queryStr, (err, data) => {
+    if (err) {
+      throw err;
+    } else {
+      callback(data);
+    }
+  });
+};
+
+const deleteById = function getAllReviewsOfRestaurant(restID, callback) {
+  const queryStr = `DELETE FROM reviews WHERE id = ${restID};`;
+  db.query(queryStr, restID, (err, data) => {
+    if (err) {
+      throw err;
+    } else {
+      callback(data);
+    }
+  });
+};
+
+const updateById= function getAllReviewsOfRestaurant(restID, callback) {
+  const queryStr = `UPDATE reviews SET message = 'Veritas vos liberabit. Audi vide tace. Lorem ipsum dolor. Doler libero fuga.' WHERE id = ${restId}`
+
+  db.query(queryStr, restID, (err, data) => {
+    if (err) {
+      throw err;
+    } else {
+      callback(data);
+    }
+  });
+};
+
 module.exports = {
   getById,
 };
